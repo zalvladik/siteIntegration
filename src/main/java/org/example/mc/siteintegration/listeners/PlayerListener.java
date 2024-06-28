3c82e49f591a21e1ca5704518287c8e243298843
VladislavZ <zalipkav@ukr.net>
2024-06-28 16:46:26 +0300

    master

--- src/main/java/org/example/mc/siteintegration/listeners/PlayerListener.java
+++ src/main/java/org/example/mc/siteintegration/listeners/PlayerListener.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
